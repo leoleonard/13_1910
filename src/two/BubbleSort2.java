752b2da3f656c541b4a332c3e14808ae28120adf
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-19 19:07:34 +0200

    add sortowanie2

---
+++ src/two/BubbleSort2.java
@@ -0,0 +1,66 @@
+package two;
+
+import two.Computer2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSort2 {
+
+    public static void main(String[] args) {
+
+        Computer2[] computers = new Computer2[]{
+                new Computer2("Dell", 2600, 8),
+                new Computer2("Asus", 2600, 8),
+                new Computer2("Dell", 2000, 16),
+                new Computer2("Predator", 4000, 16),
+                new Computer2("Dell", 2600, 6)
+        };
+
+        boolean whereChanges;
+
+        do {
+            whereChanges = false;
+            for (int i = 0; i < computers.length - 1; i++) {
+                Computer2 c1 = computers[i];
+                Computer2 c2 = computers[i + 1];
+
+                int compareResult = compareTo(c1, c2);
+
+                if (compareResult > 0) {
+                    swapElements(computers, i);
+                    whereChanges = true;
+                }
+
+            }
+        } while (whereChanges);
+
+
+        String arrayAsString = Arrays.toString(computers);
+        System.out.println(arrayAsString);
+
+    }
+
+    private static int compareTo(Computer2 c1, Computer2 c2) {
+        if(c1.getCpuMhz() > c2.getCpuMhz()) {
+            return 1;
+        } else if(c1.getCpuMhz() == c2.getCpuMhz()) {
+
+            if(c1.getRam() > c2.getRam()) {
+                return 1;
+            } else if(c1.getRam() == c2.getRam()) {
+                return c1.getName().compareTo(c2.getName());
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    private static void swapElements(Computer2[] computers, int i) {
+        Computer2 temp = computers[i];
+        computers[i] = computers[i + 1];
+        computers[i + 1] = temp;
+    }
+}
---
+++ src/two/Computer2.java
@@ -0,0 +1,67 @@
+package two;
+
+public class Computer2 implements Comparable<Computer2> {
+
+    private String name;
+    private int cpuMhz;
+    private int ram;
+
+    public Computer2(String name, int cpuMhz, int ram) {
+        this.name = name;
+        this.cpuMhz = cpuMhz;
+        this.ram = ram;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCpuMhz() {
+        return cpuMhz;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpuMhz(int cpuMhz) {
+        this.cpuMhz = cpuMhz;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "name='" + name + '\'' +
+                ", cpuMhz=" + cpuMhz +
+                ", ram=" + ram +
+                "}\n";
+    }
+
+    @Override
+    public int compareTo(Computer2 o) {
+        if(this.cpuMhz > o.cpuMhz) {
+            return 1;
+        } else if(this.cpuMhz == o.cpuMhz) {
+
+            if(this.ram > o.ram) {
+                return 1;
+            } else if(this.ram == o.ram) {
+                return this.name.compareTo(o.name);
+            } else {
+                return -1;
+            }
+
+
+        } else {
+            return -1;
+        }
+    }
+}
